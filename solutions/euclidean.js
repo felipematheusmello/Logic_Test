734d48031505e3d51637e4795139840d686bb3bf
felipe <felipematheusmellodemorais@gmail.com>
2020-12-10 15:48:23 -0300

    feat: the last one is done

--- solutions/euclidean.js
+++ solutions/euclidean.js
@@ -1,20 +1,30 @@
 /* 
-N = números de círculos de 0 até n-1
-[0, n-1 ]
-
-Não entendi muito bem esse último desafio  =/
+       O que entendi ;D
+Vou ter que passar de acordo com o módulo (x + m) modulo N (%)
+e quando chegar a um chocolate que já foi comido eu vou parar
+a execução
 */
 
 const solution = (N, M) => {
-  let chocolateNumber = N - 1;
-  let wrappers = M;
+  let chocolates = [];
+  let nextChocolate = 0;
+  for (let i = 0; i < N - 1; i++) {
+    if (!chocolates.includes(nextChocolate)) {
+      chocolates.push(nextChocolate);
+      nextChocolate = (nextChocolate + M) % N;
+    }
+  }
 
-  return chocolateNumber - wrappers;
+  return chocolates.join(",");
 };
 
 /* 
-peguei o número de doces e julguei o M como o número
-de Wrappers pela representação do número de chocolates X + M,
- ou seja chocolate que a pessoa já comeu mais o número de 
- embrulhos que daria o próximo chocolate.
+          Como fiz *.*
+Fiz um for para iterar sobre a quantidade 
+de chocolates dentro do circulo que como 
+no enúnciado falava iterava do 0 até o n-1
+e para pegar o próximo chocolate eu usei a
+formula (chocolate à ser comido + embrulho) % 
+número de chocolates
+
 */
